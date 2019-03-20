1d0fb72806c35d044b0efe6ad9a1b05ff42446df
maoyueyue <1660478286@qq.com>
2019-03-20 22:04:54 +0800

    uni-app第一次真机练习

---
+++ first-uni-app/pages/ucenter/ucenter.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="content">
+		<view>
+			<text class="title">{{title}}</text>
+				<navigator url="setting" hover-class="navigator-hover">
+		
+			<button type="primary">个人设置</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '6666'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
